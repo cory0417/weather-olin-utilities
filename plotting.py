30ef89a670e4f613b777b192c6d821f277e5f361
Daeyoung Kim <corydykim@gmail.com>
2023-03-26 13:37:32 -0400

    clean up the imports

--- plotting.py
+++ plotting.py
@@ -1,8 +1,7 @@
-import functions
+from functions import flatten_json
 import pandas as pd
 
-df_avg_temp = functions.flatten_json("TAVG")
-df_total_precp = functions.flatten_json("PRCP")
-
+df_avg_temp = flatten_json("TAVG")
+df_total_precp = flatten_json("PRCP")
+df_avg_wind = flatten_json("AWND")
 df_util = pd.read_csv("./data/electricity_FY13_23.csv")
-print(functions.join_dataframes(df_avg_temp, df_util))
