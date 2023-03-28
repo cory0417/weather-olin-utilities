e28884306450240df5415ab27f7fe4122513b788
Daeyoung Kim <corydykim@gmail.com>
2023-03-28 18:59:50 -0400

    Remove plotting of data from the file

--- plotting.py
+++ plotting.py
@@ -1,4 +1,4 @@
-from functions import flatten_json, plot_data, join_dataframes
+from functions import flatten_json, join_dataframes
 import pandas as pd
 
 df_avg_temp = flatten_json("TAVG")
@@ -7,5 +7,3 @@ df_avg_wind = flatten_json("AWND")
 df_util = pd.read_csv("./data/electricity_FY13_23.csv")
 df_util_wind = join_dataframes(df_avg_wind, df_util)
 df_util_temp = join_dataframes(df_avg_temp, df_util)
-
-plot_data(df_util_temp)
