9460d7f9c92d0c93e78db53bfda6e67aaf5d11c1
Ellen Sun <sunellenyuan@gmail.com>
2023-03-30 17:49:41 -0400

    Add pytest for filtering seasons function

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,11 +1,19 @@
+"""
+File to run pytest unit tests on different helper functions in functions.py
+"""
+
+from os import path
+import pandas as pd
+import pytest
+
 from functions import (
     merge_all_df,
     feature_selection,
     get_data_api,
     flatten_json,
+    filter_season,
+    join_dataframes,
 )
-from os import path
-import pytest
 
 
 @pytest.fixture(scope="session")
@@ -29,6 +37,29 @@ def input_results():
     print("test")
     print(df_results)
     return df_results
+
+
+@pytest.fixture(scope="session")
+def create_df():
+    """
+    Creates a dataframe containing all weather and utility information
+    of the TAVG datatype for testing purposes.
+
+    Args:
+        Nothing
+    Returns:
+        A pandas dataframe containing all weather and utility information
+        about the TAVG datatype.
+    """
+    if path.isfile("data/TAVG.json"):
+        pass
+    else:
+        get_data_api("TAVG")
+
+    df_avg_temp = flatten_json("TAVG")
+    df_util = pd.read_csv("./data/electricity_FY13_23.csv")
+    df_util_temp = join_dataframes(df_avg_temp, df_util)
+    return df_util_temp
 
 
 def test_f_score(input_results):
@@ -57,3 +88,25 @@ def test_p_value(input_results):
         with "p-value" as one of its columns.
     """
     assert (input_results["p-value"] >= 0).all()
+
+
+filter_season_cases = [
+    ("Winter", ["12", "01", "02"]),
+    ("Spring", ["03", "04", "05"]),
+    ("Summer", ["06", "07", "08"]),
+    ("Fall", ["09", "10", "11"]),
+]
+
+
+@pytest.mark.parametrize("season_name,season_nums", filter_season_cases)
+def test_filter_season(season_name, season_nums, create_df):
+    """
+    Check that the returned dataframe actually filters out the right months
+    for the given season.
+    """
+    filtered_df = filter_season(season_name, create_df)
+    assert all(
+        pd.to_datetime(filtered_df["year-month"])
+        .dt.strftime("%m")
+        .isin(season_nums)
+    )
